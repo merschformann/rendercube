9604e412ddccaba2f2871cd15c038fdb20c51ae2
Marius Merschformann <marius.merschformann@gmail.com>
2024-01-09 02:22:43 +0100

    Cleaning up, update readme

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # rendercube
 
-Renders simple shapes stored in JSON via Three.js
+Renders [SardineCan](https://github.com/merschformann/sardine-can) solutions stored in JSON via Three.js as a self-contained HTML file.
+
+> **Note:** This is a work in progress with very limited functionality.
+
+## Installation
+
+```bash
+go install github.com/merschformann/rendercube
+```
+
+## Usage
+
+```bash
+rendercube -input <input.json> -output <output.html>
+```
--- rendering/convert.go
+++ rendering/convert.go
@@ -10,33 +10,16 @@ import (
 //go:embed template.html
 var template string
 
+// TargetLength is the length to which the solution is resized.
+const TargetLength = 1000
+
+// Convert creates a HTML file from the given input.
 func Convert(input Input) (string, error) {
 	// Collect some information about the solution
 	pieces, containers := 0, 0
 	for i := range input.Containers {
 		containers++
 		pieces += len(input.Containers[i].Assignments)
-	}
-
-	// Downsize lengths
-	factor := 1.0
-	for i := range input.Containers {
-		input.Containers[i].Length /= factor
-		input.Containers[i].Width /= factor
-		input.Containers[i].Height /= factor
-		for j := range input.Containers[i].Assignments {
-			input.Containers[i].Assignments[j].Position.X /= factor
-			input.Containers[i].Assignments[j].Position.Y /= factor
-			input.Containers[i].Assignments[j].Position.Z /= factor
-			for k := range input.Containers[i].Assignments[j].Cubes {
-				input.Containers[i].Assignments[j].Cubes[k].X /= factor
-				input.Containers[i].Assignments[j].Cubes[k].Y /= factor
-				input.Containers[i].Assignments[j].Cubes[k].Z /= factor
-				input.Containers[i].Assignments[j].Cubes[k].Length /= factor
-				input.Containers[i].Assignments[j].Cubes[k].Width /= factor
-				input.Containers[i].Assignments[j].Cubes[k].Height /= factor
-			}
-		}
 	}
 
 	// Write output
